01304beb74afec51cb462c9a31bedfc674ded597
avierb11 <54750586+avierb11@users.noreply.github.com>
2021-01-12 15:42:08 -0600

    Update groundwater functions

---
+++ ModellingProgram/EarthScience.py
@@ -0,0 +1,14 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.lang import Builder
+
+#builder = Builder()
+
+#builder.load_file("EarthScience.kv")
+
+class EarthScience(App):
+    pass
+
+if __name__ == '__main__':
+    EarthScience().run()
